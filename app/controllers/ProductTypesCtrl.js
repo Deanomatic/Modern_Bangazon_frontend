e1c2eef54ab291251358f5eacf6e83a8f2b9e56c
Dean Smith <Dean Smith>
2017-06-08 22:52:15 -0500

    Added more angular

--- app/controllers/ProductTypesCtrl.js
+++ app/controllers/ProductTypesCtrl.js
@@ -1,5 +1,29 @@
 "use strict";
 
 angular.module("Bangazon_Client")contorller("ProductTypesController", {[
+	'$scope', 
+  '$http', 
+  '$location', 
+  'RootFactory', 
+  function($scope, $http, $location, RootFactory) {
+    $scope.product_types = [];
+
+    RootFactory.getApiRoot()
+      .then(
+        root => 
+          $http({
+            url: `${root.product_types}`,
+            method: "GET",
+            headers: {
+              'Authorization': "Token " + RootFactory.getToken()
+            }
+          })
+          .then(
+            res => $scope.product_types = res.data.results,
+            console.log
+          )
+        ,console.log
+      );
+  	}
 
 	]})
---
+++ app/controllers/ProductsCtrl.js
@@ -0,0 +1,28 @@
+"use strict";
+
+angular.module('Bangazon_Client').controller('ProductController', [
+  '$scope', 
+  '$http', 
+  '$location', 
+  'RootFactory', 
+  function($scope, $http, $location, RootFactory) {
+    $scope.products = [];
+
+    RootFactory.getApiRoot()
+      .then(
+        root => 
+          $http({
+            url: `${root.products}`,
+            headers: {
+              'Authorization': "Token " + RootFactory.getToken()
+            }
+          })
+          .then(
+            res => $scope.products = res.data.results,
+            err => console.log
+          )
+        ,err => console.log
+      );
+  }
+]);
+
